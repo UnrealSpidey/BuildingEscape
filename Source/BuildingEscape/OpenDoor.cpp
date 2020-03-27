b45f0bb97d815173e0412fafb45fa1aaa867fbb9
Unreal Spidey <nick.bullardbradley@icloud.com>
2020-03-27 19:57:46 -0400

    Completed 98. Rotating An Actor With Code
    Completed code and content portion of this lecture.

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,46 @@
+// Copyright Nick Bullard
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString ActorName = GetOwner()->GetName();
+
+	FRotator ActorCurrentRotation = GetOwner()->GetActorRotation();
+	//UE_LOG(LogTemp, Warning, TEXT("%s closed rotation %s"), *ActorName, *ActorCurrentRotation.ToString());
+	ActorCurrentRotation.Yaw = 90.f;
+	//FRotator OpenDoor = { 0.f, 90.f, 0.f };
+	//FRotator OpenDoor(0.f, 90.f, 0.f);
+	
+	GetOwner()->SetActorRotation(ActorCurrentRotation);
+	
+	
+	UE_LOG(LogTemp, Warning, TEXT("%s open rotation %s"), *ActorName, *ActorCurrentRotation.ToString());
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
